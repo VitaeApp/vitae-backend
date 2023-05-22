bc1659201e79ffcbecc7e992d348e1db358b3e38
snifhex <sachintripathis84@gmail.com>
2023-05-22 16:09:52 +0530

    BLD: Updated scripts in package.json

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,10 +1,8 @@
 import express from "express";
 const app = express();
-
 app.get("/", (req, res) => {
-  res.send("Hello Vitae");
+    res.send("Hello from express and typescript");
 });
-
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
+//# sourceMappingURL=index.js.map
